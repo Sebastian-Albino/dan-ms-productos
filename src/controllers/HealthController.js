87d60d2bf4956f45e8d5b680cabc3cabd32f353a
Sebastian Albino <sebalbino777@gmail.com>
2024-08-04 12:01:03 -0300

    Configuracion de Eureka

--- src/controllers/HealthController.js
+++ src/controllers/HealthController.js
@@ -3,11 +3,10 @@ import { json } from "express";
 async function mostrarEstado(req, res){
  
     try{
-        res.json({
+        res.status(200).json({
             app: "ms-productos",
             status: "OK"
         });
-        return res.status(200).json();
     }catch(error){
         return res.status(500).json({
             error: error.message
